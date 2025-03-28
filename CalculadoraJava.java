0abfb95a9999c24e9c43ee33e108317327e1def0
Bruna Guimarães <103650184+bruna-gguimaraes@users.noreply.github.com>
2025-03-28 03:31:01 -0300

    Update CalculadoraJava.java

--- CalculadoraJava.java
+++ CalculadoraJava.java
@@ -8,28 +8,28 @@ public class CalculadoraJava {
       System.out.print("Digite o segundo número: ");
       double num2 = scanner.nextDouble();
       System.out.print("Digite a operação (+, -, *, /): ");
-      char operator = scanner.next().charAt(0);
+      char operador = scanner.next().charAt(0);
       scanner.close();
-      double result;
+      double resulto;
 
-      switch(operator) {
+      switch(operador) {
          case '+':
-            result = num1 + num2;
+            resulto = num1 + num2;
             break;
          case '-':
-            result = num1 - num2;
+            resulto = num1 - num2;
             break;
          case '*':
-            result = num1 * num2;
+            resulto = num1 * num2;
             break;
          case '/':
-            result = num1 / num2;
+            resulto = num1 / num2;
             break;
          default:
             System.out.println("Operação inválida.");
             return;
       }
 
-      System.out.println(num1+" "+operator+" "+num2+": "+result);
+      System.out.println(num1+" "+operador+" "+num2+": "+resulto);
    }
 }
